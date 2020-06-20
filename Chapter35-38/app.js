e139408d8e9fa613e3e6d0cf312d04d967fe3bd7
syedrafayhashmi <rafaysidsid@gmail.com>
2020-06-21 00:17:19 +0500

    assignment 35-38

--- Chapter35-38/app.js
+++ Chapter35-38/app.js
@@ -7,7 +7,47 @@ function dateTime(){
 
 // Task 2
 function greet(){
-   var first = prompt("Enter First Name");
+   var first = prompt("Enter First Name")+" ";
    var last = prompt("Enter Last Name");
+   var fullName = first.concat(last)
+   return document.write("Hello "+fullName+"<br>");
+}
+//greet();
 
+// Task 3
+function sum(){
+    var num1 = +prompt("Enter number 1");
+    var num2 = +prompt("Enter number 2");
+    return document.write(num1+num2);
+}
+//sum();
+
+// Task 4
+function calculator(num1,num2,operator){
+     if(operator === "+"){
+        return document.write(num1+num2);   
+    }
+    if(operator === "-"){
+        return document.write(num1-num2);
+    }
+    if(operator === "*"){
+        return document.write(num1*num2);
+    }
+    if(operator === "/"){
+        return document.write(num1/num2);
+    }
+    if(operator === "%"){
+        return document.write(num1%num2);
+    }
+    else{
+        return document.write("Wrong Input");
+    }
+}
+//calculator(5,4,"+");
+//calculator(40,8,"/");
+//calculator(5,4,"*");
+
+// Task 5
+function square(num){
+    
 }
